930fc1ef8fbe9b46e5a6e50452c6ce7981d2fa86
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2026-01-21 21:35:05 -0500

    list active jobs

--- job_board/views.py
+++ job_board/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import JobPosting
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Job Board")
+    active_postings = JobPosting.objects.filter(is_active=True)
+    context = {
+        'job_postings': active_postings
+    }
+    return render(request, 'job_board/index.html', context)
