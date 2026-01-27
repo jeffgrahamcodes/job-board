1d27eb541290a444f4c7d45b8afaf91bc7864b1a
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2026-01-27 15:37:56 -0500

    Handle 'does not exist' errors

--- job_board/views.py
+++ job_board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import JobPosting
 
@@ -11,7 +11,7 @@ def index(request):
     return render(request, 'job_board/index.html', context)
 
 def job_detail(request, pk):
-    job_posting = JobPosting.objects.get(pk=pk)
+    job_posting = get_object_or_404(JobPosting, pk=pk, is_active=True)
     context = {
         'posting': job_posting
     }
